0ee3ec50b7538a4ca8a2007810e9d7722c19b608
h0nt3d <akanoim@gmail.com>
2025-04-20 22:25:50 -0300

    Added Delay

--- pyramid.c
+++ pyramid.c
@@ -1,18 +1,30 @@
+#include <time.h>
 #include <stdio.h>
 
-void drawPyramid(int limit) {
+void delay(int seconds) 
+{
+	int milliSeconds = 1000 * seconds;
+	clock_t startTime = clock();
+	while (clock() < startTime + milliSeconds);
+}
+
+void drawPyramid(int limit) 
+{
 	char text = '|';
 	for (int i = 1; i < limit; i++) {
 		int temp = 0;
 		while (temp < (limit - limit + i)) {
+			delay(5);
 			printf("%c", text);
 			temp++;
 		}
+		delay(5);
 		printf("\n");
 	}
 }
 
-int main() {
+int main() 
+{
 	drawPyramid(10);
 	return 0;
 }
